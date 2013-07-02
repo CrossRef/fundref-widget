bae3c534e4e106e2e2c9b737e4ad47bfca6d2d6f
Karl Jonathan Ward <karl.j.ward@gmail.com>
2013-07-02 11:35:26 +0100

    Initial commit.

---
+++ js/main.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+  var funderEntryCount = 0;
+  var grantNumberEntryCounts = {};
+
+  var makeSubsidiaryOptions = function(descendants, names) {
+      var html = '<option>Select a sub-organization</option>';
+      $.each(descendants, function(idx, d) {
+	  html += '<option>' + names[d] + '</option>';
+      });
+      return html;
+  };
+
+  var addNewGrantNumber = function(funderEntryNumber) {
+    grantNumberEntryCounts[funderEntryNumber] = grantNumberEntryCounts[funderEntryNumber] + 1;
+    var cssPath = '.funder-entry.seq-' + funderEntryNumber + ' ';
+    var $newEntry = $('.grant-number-entry.seq-0').first().clone();
+    $newEntry.removeClass('seq-0').addClass('seq-' + grantNumberEntryCounts[funderEntryCount]);
+    $newEntry.show();
+    $(cssPath + '#grant-number-entries').append($newEntry);
+  }
+
+  var applyFunderEntryCallbacks = function(sequenceNumber) {
+    var cssPath = '.funder-entry.seq-' + sequenceNumber + ' ';
+    var events = 'typeahead:autocompleted typeahead:selected';
+
+    $(cssPath + '#funder-name').typeahead({
+      name: 'funders',
+      remote: 'http://search.crossref.org/funders?descendants=true&q=%QUERY',
+      limit: 16
+    });
+
+    $(cssPath + '#funder-name').bind(events, function(e, datum) {
+      if (datum['descendants'].length > 1) {
+	$(cssPath + '#subsidiary-select').html(makeSubsidiaryOptions(datum['descendants'], datum['descendant_names']));
+	$(cssPath + '#subsidiary-list').show();
+      } else {
+	$(cssPath + '#subsidiary-select').html('');
+        $(cssPath + '#subsidiary-list').hide();
+      }
+    });
+
+    $(cssPath + '#add-grant-number-link').click(function(e) {
+      addNewGrantNumber(sequenceNumber);
+    });
+  }
+
+  var addNewFunder = function() {
+    funderEntryCount = funderEntryCount + 1;
+    var $newEntry = $('.funder-entry.seq-0').first().clone();
+    $newEntry.removeClass('seq-0').addClass('seq-' + funderEntryCount);
+    $newEntry.show();
+    $('#funder-entries').append($newEntry);
+    grantNumberEntryCounts[funderEntryCount] = 0;
+    addNewGrantNumber(funderEntryCount);
+    applyFunderEntryCallbacks(funderEntryCount);
+  }
+
+  $('#add-funder-link').click(function(e) {
+    addNewFunder();
+  });
+
+  addNewFunder();
+});
